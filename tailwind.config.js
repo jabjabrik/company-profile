830fa76cab22c122670a1ff21ceb7902a8b84bba
MochYusuf <yusufjabriko@gmail.com>
2024-03-05 23:49:36 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["src/pages/*.html"],
+    content: ["index.html", 'tentang.html', 'layanan.html', 'produk.html', 'kontak.html'],
     // content: ["*.html", "./node_modules/flowbite/**/*.js"],
     theme: {
         extend: {}
